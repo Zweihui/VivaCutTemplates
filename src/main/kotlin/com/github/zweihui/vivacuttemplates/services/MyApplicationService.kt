c02ba00286f49df124fb7b3b52e04c53a52f2ebf
GitHub Action <action@github.com>
2021-03-30 03:32:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/zweihui/vivacuttemplates/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zweihui/vivacuttemplates/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zweihui.vivacuttemplates.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zweihui.vivacuttemplates.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/zweihui/vivacuttemplates/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zweihui/vivacuttemplates/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zweihui.vivacuttemplates.services
 
+import com.github.zweihui.vivacuttemplates.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
